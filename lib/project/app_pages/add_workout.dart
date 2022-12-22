b2e8896ac2c9f87131f03ab9f0b24614e6dd5b8e
Xasthul <nazargon2003@gmail.com>
2022-12-22 22:36:11 +0200

    Added popup menu

--- lib/project/app_pages/add_workout.dart
+++ lib/project/app_pages/add_workout.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 
-class AddWorkout extends StatelessWidget {
+class AddWorkout extends StatefulWidget {
   const AddWorkout({Key? key}) : super(key: key);
+
+  static const menuItems = <String>['Add exercise'];
+
+  @override
+  State<AddWorkout> createState() => _AddWorkoutState();
+}
+
+class _AddWorkoutState extends State<AddWorkout> {
+  final List<PopupMenuItem<String>> _popupMenuItems = AddWorkout.menuItems.map(
+      (String value) => PopupMenuItem<String>(
+        value: value,
+          child: Text(value))
+  ).toList();
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +23,22 @@ class AddWorkout extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blue[400],
         title: const Text("Add Workout", style: TextStyle(fontSize: 21)),
-        centerTitle: true,
+        //centerTitle: true,
         actions: <Widget>[
-          IconButton(
-            tooltip: "Menu",
-            icon: const Icon(Icons.more_vert),
-            onPressed: () {},
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                  value: 1,
+                  child: Text("Add new exercise"),
+                  )
+            ],
+            offset: const Offset(0, 60),
           )
+          // IconButton(
+          //   tooltip: "Menu",
+          //   icon: const Icon(Icons.more_vert),
+          //   onPressed: () {},
+          // )
         ],
       ),
     );
