36587c79592beb780eb6b2be1f2cc875f5c59ae2
Xasthul <nazargon2003@gmail.com>
2022-12-25 20:21:33 +0200

    Added functionality to add exercises

--- lib/project/app_pages/exercises.dart
+++ lib/project/app_pages/exercises.dart
@@ -8,6 +8,21 @@ class Exercises extends StatefulWidget {
 }
 
 class _ExercisesState extends State<Exercises> {
+  late TextEditingController _controller;
+  List<String> _exercises = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,15 +36,46 @@ class _ExercisesState extends State<Exercises> {
             icon: const Icon(Icons.search),
           ),
           IconButton(
-              tooltip: "Add new exercise",
-              onPressed: () {},
-              icon: const Icon(Icons.add),
+            tooltip: "Add new exercise",
+            onPressed: () async {
+              final newExerciseName = await openDialog();
+              if (newExerciseName == null || newExerciseName.isEmpty) return;
+              setState(() {
+                _exercises.add(newExerciseName);
+              });
+            },
+            icon: const Icon(Icons.add),
           ),
-
         ],
-        //centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: _exercises.length,
+        itemBuilder: (context, index) => Text(_exercises[index]),
       ),
     );
   }
-}
 
+  Future<String?> openDialog() => showDialog<String>(
+      context: context,
+      builder: (context) => AlertDialog(
+            title: const Text("New exercise"),
+            content: TextField(
+              autofocus: true,
+              controller: _controller,
+              decoration: const InputDecoration(hintText: "Name of exercise"),
+              onSubmitted: (_) => submitDialog(),
+            ),
+            actions: [
+              TextButton(
+                onPressed: submitDialog,
+                child: const Text("Add"),
+              )
+            ],
+          ));
+
+  void submitDialog() {
+    Navigator.of(context).pop(_controller.text);
+
+    _controller.clear();
+  }
+}
